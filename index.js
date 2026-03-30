69635cb54cbe90d3de26e11475595c6a2c31bf9f
Brandon Sosa <brandonescsosa@gmail.com>
2026-03-29 22:06:13 -0700

    Implementado endpoint /usuario con JSON

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
+// Ruta base
 app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+  res.send('API de Brandon Sosa funcionando');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+// NUEVO: Endpoint /usuario
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Brandon Everardo Escamilla Sosa',
+    rol: 'Estudiante de Informática UAS',
+    semestre: '6to'
+  };
+
+  // Regresa el objeto en formato JSON
+  res.json(usuario);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
